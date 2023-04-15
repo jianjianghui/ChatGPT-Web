84c08d9b888a9d6a2d8b0b965c0220cefceb9e28
jianjianghui <jianjianghui@foxmail.com>
2023-04-16 01:33:23 +0800

    支持netlify云平台

--- start-netlify.sh
+++ start-netlify.sh
@@ -1,15 +1,6 @@
 
-cd ./service
 pnpm install
-# 打包
-pnpm build
 
-# 运行
-nohup sh -c 'export OPENAI_ACCESS_TOKEN=${OPENAI_ACCESS_TOKEN} && pnpm prod > service.log && netlify build' &
-echo "Start service complete!"
-
-
-cd ..
 echo "" > front.log
 pnpm install
 nohup pnpm build > front.log &
